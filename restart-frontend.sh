72e14b72f1e75181ad7a293e826461a873707d0d
koushik.chandra <koushik.chandra@bracits.com>
2025-09-29 09:06:27 +0600

    update port

--- restart-frontend.sh
+++ restart-frontend.sh
@@ -7,5 +7,5 @@ docker rm -f kidora-frontend || true
 
 echo "👉 Running new container..."
 # Run new container
-docker run -d -p 8080:80 --network kidora-deployment_kidora-network --name kidora-frontend kidora-frontend
+docker run -d -p 80:80 --network kidora-deployment_kidora-network --name kidora-frontend kidora-frontend
 echo "✅ Frontend Running on port 8080"
